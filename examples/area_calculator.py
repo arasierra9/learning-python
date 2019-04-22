e34221ccfb16cf54e7ded7ffbc54065b3084cc6d
Araceli M. SIERRA <arasierra9@gmail.com>
2019-04-21 23:51:00 -0300

    UPD // Area Calculator

---
+++ examples/area_calculator.py
@@ -0,0 +1,24 @@
+""""
+Area Calculator Example
+Author: Araceli M. SIERRA
+"""
+
+from __future__ import print_function
+raw_input = input
+
+print("Welcome, this is the begginning...")
+name = raw_input("What's your name?")
+option = raw_input('Enter C for "Circle" or T for "Triangle": ').upper()
+
+if option == "C":
+  radius = float(raw_input("Enter radius: "))
+  area = 3.14 * (radius ** 2)
+  print("Circle base is: %s" % area)
+elif option == "T":
+  base = float(raw_input("Enter base: "))
+  height = float(raw_input("Enter height: "))
+  area = .5 * base * height
+  print("Triangle base is: %s" % area)
+else:
+  print("Value not found: %s" % option)
+print('Exiting...')

4a0dcc4501f20396145b2d804d51465b0c262e26
Alejandro M. Bernardis <alejandro.m.bernardis@gmail.com>
2019-04-22 20:11:37 -0300

    UPD // More examples

--- examples/area_calculator.py
+++ examples/area_calculator.py
@@ -11,14 +11,15 @@ name = raw_input("What's your name?")
 option = raw_input('Enter C for "Circle" or T for "Triangle": ').upper()
 
 if option == "C":
-  radius = float(raw_input("Enter radius: "))
-  area = 3.14 * (radius ** 2)
-  print("Circle base is: %s" % area)
+	radius = float(raw_input("Enter radius: "))
+	area = 3.14 * (radius ** 2)
+	print("Circle base is: %s" % area)
 elif option == "T":
-  base = float(raw_input("Enter base: "))
-  height = float(raw_input("Enter height: "))
-  area = .5 * base * height
-  print("Triangle base is: %s" % area)
+	base = float(raw_input("Enter base: "))
+	height = float(raw_input("Enter height: "))
+	area = .5 * base * height
+	print("Triangle base is: %s" % area)
 else:
-  print("Value not found: %s" % option)
+	print("Value not found: %s" % option)
+	
 print('Exiting...')
--- examples/max1.py
+++ examples/max1.py
@@ -1,5 +1,12 @@
 # Set maximum to the max value of any set of numbers on line 3!
 
 maximum = max(12, 13, 14, 15, 16)
-
 print(maximum)
+
+# limit
+
+mx = 10
+mi = 1
+
+for x in (1,22,3,42,5,11,9,):
+	print(x, max(min(x, mx), mi))

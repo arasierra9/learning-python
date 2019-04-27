ecaa6685977d71a04c59f84f49e4e23f1ea8d3a7
Araceli M. SIERRA <arasierra9@gmail.com>
2019-04-26 23:12:39 -0300

    Planning your trip

--- examples/Planning your trip.py
+++ examples/Planning your trip.py
@@ -1,3 +1,14 @@
 def hotel_cost(nights):
     # Hotel per night is $140
     return 140 * nights
+
+
+def plane_ride_cost(city):
+  if city == "Charlotte":
+    return 183
+  elif city == "Tampa":
+    return 220
+  elif city == "Pittsburgh":
+    return 222
+  elif city == "Los Angeles":
+    return 475
